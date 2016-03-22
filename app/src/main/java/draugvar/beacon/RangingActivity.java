fa44cd8d5bec049c2fcf95d3a8bc8966588bd501
Sara <saraveterini@gmail.com>
2016-03-22 11:12:18 +0100

    immagine circolare

--- app/src/main/java/draugvar/beacon/RangingActivity.java
+++ app/src/main/java/draugvar/beacon/RangingActivity.java
@@ -3,6 +3,7 @@ package draugvar.beacon;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.RemoteException;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.mikepenz.fastadapter.adapters.FastItemAdapter;
@@ -18,18 +19,20 @@ import java.util.Collection;
 public class RangingActivity extends Activity implements BeaconConsumer {
     protected static final String TAG = "RangingActivity";
     private BeaconManager beaconManager;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranging);
+        Log.i(TAG, "RANGING ");
         beaconManager = BeaconManager.getInstanceForApplication(this);
         // To detect proprietary beacons, you must add a line like below corresponding to your beacon
         // type.  Do a web search for "setBeaconLayout" to get the proper expression.
         // beaconManager.getBeaconParsers().add(new BeaconParser().
         //        setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
         beaconManager.bind(this);
-
+        recyclerView=(RecyclerView)findViewById(R.id.beacon_recycler_view);
         //create our FastAdapter which will manage everything
         FastItemAdapter fastAdapter = new FastItemAdapter();
 
